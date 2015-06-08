551c80b62142a4866afa85ea9a3e92dba9f2b896
Ethan Yonker <dees_troy@teamw.in>
2015-06-08 09:57:49 -0500

    Updates

--- device.mk
+++ device.mk
@@ -40,7 +40,8 @@ PRODUCT_PACKAGES += \
         librs_jni
 
 PRODUCT_COPY_FILES += \
-	device/oppo/find5/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat
+	device/oppo/find5/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+	device/oppo/find5/kernel:kernel
 
 PRODUCT_COPY_FILES += \
 	device/oppo/find5/configs/snd_soc_msm_2x_Fusion3:system/etc/snd_soc_msm/snd_soc_msm_2x_Fusion3 \
